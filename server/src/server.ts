a6695911dee015fe38b1bdc636692ee1bbd85da9
Bruno Castro <miroldols@gmail.com>
2022-05-10 09:42:58 -0300

    Updated server port

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,6 +12,8 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => {
-  console.log('Server started on port 3333');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
